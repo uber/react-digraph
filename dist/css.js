9578bd517139f0a08235aba294899d9633001a40
Allan Bogh <ajbogh@uber.com>
2018-09-12 13:50:18 -0700

    Added test message

--- dist/css.js
+++ dist/css.js
@@ -65,7 +65,7 @@ var ReactDigraph =
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f62c00c1272f2dc03f02";
+/******/ 	var hotCurrentHash = "a7e758ced0328a08db22";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/components/graph-view.js
+++ src/components/graph-view.js
@@ -423,9 +423,10 @@ class GraphView extends React.Component<IGraphViewProps, IGraphViewState> {
 
   handleWrapperKeydown: KeyboardEventListener = (d) => {
     // Conditionally ignore keypress events on the window
-    if (!document.activeElement === ReactDOM.findDOMNode(this.refs.view)) {
+    if (document.activeElement !== ReactDOM.findDOMNode(this.refs.view)) {
       return;
     }
+    console.log(document.activeElement, ReactDOM.findDOMNode(this.refs.view));
     switch (d.key) {
       case 'Delete':
       case 'Backspace':
