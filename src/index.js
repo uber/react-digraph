21518352c1cb03b4b594f230e6c41b8afda96070
Mikhail Khodos <mkhodos@trifacta.com>
2019-11-11 21:24:07 +0000

    Eslint

--- src/index.js
+++ src/index.js
@@ -20,17 +20,13 @@ import type { IEdge } from './components/edge';
 import type { INode } from './components/node';
 
 export { default as GraphViewFast } from './components/graph-view';
-export {
-  default as LayoutEngines,
-} from './utilities/layout-engine/layout-engine-config';
+export { default as LayoutEngines } from './utilities/layout-engine/layout-engine-config';
 export { default as Edge } from './components/edge';
 export type IEdgeType = IEdge;
 export { default as GraphUtils } from './utilities/graph-util';
 export { default as Node } from './components/node';
 export type INodeType = INode;
-export {
-  default as BwdlTransformer,
-} from './utilities/transformers/bwdl-transformer';
+export { default as BwdlTransformer } from './utilities/transformers/bwdl-transformer';
 export { GV as GraphView };
 
 export default GV;
