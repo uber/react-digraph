34888b00153f129941501278e6b14acc50430dcb
Diego Schvartzman <diego@holaemi.com>
2020-02-06 12:54:11 -0300

    ship icon

--- src/examples/emi-flow-editor/components/flow-management.js
+++ src/examples/emi-flow-editor/components/flow-management.js
@@ -184,6 +184,16 @@ class FlowManagement extends React.Component {
     );
   };
 
+  shipEnabled = () => this.state.s3stored && !this.state.unsavedChanges;
+
+  shipClasses = () => {
+    const classes = ['managerButton svg-inline--fa fa-rocket fa-w-16'];
+
+    return GraphUtils.classNames(
+      classes.concat(this.cloneEnabled() ? ['enabled'] : [])
+    );
+  };
+
   render() {
     const {
       s3Loading,
@@ -293,11 +303,28 @@ class FlowManagement extends React.Component {
             >
               <path d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path>
             </svg>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="rocket"
+              className={this.shipClasses()}
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path d="M505.12019,19.09375c-1.18945-5.53125-6.65819-11-12.207-12.1875C460.716,0,435.507,0,410.40747,0,307.17523,0,245.26909,55.20312,199.05238,128H94.83772c-16.34763.01562-35.55658,11.875-42.88664,26.48438L2.51562,253.29688A28.4,28.4,0,0,0,0,264a24.00867,24.00867,0,0,0,24.00582,24H127.81618l-22.47457,22.46875c-11.36521,11.36133-12.99607,32.25781,0,45.25L156.24582,406.625c11.15623,11.1875,32.15619,13.15625,45.27726,0l22.47457-22.46875V488a24.00867,24.00867,0,0,0,24.00581,24,28.55934,28.55934,0,0,0,10.707-2.51562l98.72834-49.39063c14.62888-7.29687,26.50776-26.5,26.50776-42.85937V312.79688c72.59753-46.3125,128.03493-108.40626,128.03493-211.09376C512.07526,76.5,512.07526,51.29688,505.12019,19.09375ZM384.04033,168A40,40,0,1,1,424.05,128,40.02322,40.02322,0,0,1,384.04033,168Z"></path>
+            </svg>
           </div>
         )}
         {!renaming ? (
           <h2
-            style={{ flex: 1, color: legacy ? 'crimson' : 'black' }}
+            style={{
+              flex: 1,
+              color: legacy ? 'crimson' : 'black',
+              marginLeft: '50px',
+              marginRight: '50px',
+            }}
             onClick={this.startRename}
           >
             {this.getDisplayName()}
