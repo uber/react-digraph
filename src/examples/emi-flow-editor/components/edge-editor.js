5e41721ff16c34b9792f70853c3a808fe9aaa74a
Diego Schvartzman <diego@holaemi.com>
2020-02-28 14:55:44 -0300

    hidden edge conditions for default connections

--- src/examples/emi-flow-editor/components/edge-editor.js
+++ src/examples/emi-flow-editor/components/edge-editor.js
@@ -85,124 +85,128 @@ class EdgeEditor extends React.Component {
             </label>
           )}
         </label>
-        <label className="inputList">
-          containsAny:
-          <ReactListInput
-            initialStagingValue=""
-            onChange={value => onChangeConn('containsAny', value)}
-            maxItems={20}
-            minItems={0}
-            ItemComponent={Item}
-            StagingComponent={StagingItem}
-            value={conn.containsAny}
-          />
-        </label>
-        <label>
-          isString:
-          <input
-            type="text"
-            name="isString"
-            value={conn.isString}
-            onChange={e => onChangeConn('isString', e.target.value)}
-          />
-        </label>
-        <label>
-          isNotString:
-          <input
-            type="text"
-            name="isNotString"
-            value={conn.isNotString}
-            onChange={e => onChangeConn('isNotString', e.target.value)}
-          />
-        </label>
-        <label>
-          lessThan:
-          <input
-            type="number"
-            name="lessThan"
-            value={conn.lessThan}
-            onChange={e => onChangeConn('lessThan', e.target.value)}
-          />
-        </label>
-        <label>
-          greaterThan:
-          <input
-            type="number"
-            name="greaterThan"
-            value={conn.greaterThan}
-            onChange={e => onChangeConn('greaterThan', e.target.value)}
-          />
-        </label>
-        <label className="inputList">
-          inArray:
-          <ReactListInput
-            initialStagingValue=""
-            onChange={value => onChangeConn('inArray', value)}
-            maxItems={20}
-            minItems={0}
-            ItemComponent={Item}
-            StagingComponent={StagingItem}
-            value={conn.inArray}
-          />
-        </label>
-        <label className="inputList">
-          notInArray:
-          <ReactListInput
-            initialStagingValue=""
-            onChange={value => onChangeConn('notInArray', value)}
-            maxItems={20}
-            minItems={0}
-            ItemComponent={Item}
-            StagingComponent={StagingItem}
-            value={conn.notInArray}
-          />
-        </label>
-        {hasIntents && (
-          <label className="inputList">
-            nlp:
-            <ReactListInput
-              initialStagingValue={{ key: null, op: null, value: '' }}
-              onChange={value =>
-                onChangeConn('nlp', this.getFilterFromItems(value))
-              }
-              maxItems={20}
-              minItems={0}
-              ItemComponent={IntentFilterItemHOC(getIntents)}
-              StagingComponent={StagingIntentFilterItemHOC(getIntents)}
-              value={getFilterItems(conn.nlp)}
-            />
-          </label>
-        )}
-        <label className="inputList">
-          answers:
-          <ReactListInput
-            initialStagingValue={{ key: null, op: null, value: '' }}
-            onChange={value =>
-              onChangeConn('answers', this.getFilterFromItems(value))
-            }
-            maxItems={20}
-            minItems={0}
-            ItemComponent={FilterItemHOC(getPrevIndexes)}
-            StagingComponent={StagingFilterItemHOC(getPrevIndexes)}
-            value={getFilterItems(conn.answers)}
-          />
-        </label>
-        <label className="inputList">
-          context:
-          <ReactListInput
-            initialStagingValue={{ key: null, op: null, value: '' }}
-            onChange={value =>
-              onChangeConn('context', this.getFilterFromItems(value))
-            }
-            maxItems={20}
-            minItems={0}
-            ItemComponent={FilterItemHOC(getSelectedNodePrevContextVars)}
-            StagingComponent={StagingFilterItemHOC(
-              getSelectedNodePrevContextVars
+        {!conn.isDefault && (
+          <div className="someNodeEditor">
+            <label className="inputList">
+              containsAny:
+              <ReactListInput
+                initialStagingValue=""
+                onChange={value => onChangeConn('containsAny', value)}
+                maxItems={20}
+                minItems={0}
+                ItemComponent={Item}
+                StagingComponent={StagingItem}
+                value={conn.containsAny}
+              />
+            </label>
+            <label>
+              isString:
+              <input
+                type="text"
+                name="isString"
+                value={conn.isString}
+                onChange={e => onChangeConn('isString', e.target.value)}
+              />
+            </label>
+            <label>
+              isNotString:
+              <input
+                type="text"
+                name="isNotString"
+                value={conn.isNotString}
+                onChange={e => onChangeConn('isNotString', e.target.value)}
+              />
+            </label>
+            <label>
+              lessThan:
+              <input
+                type="number"
+                name="lessThan"
+                value={conn.lessThan}
+                onChange={e => onChangeConn('lessThan', e.target.value)}
+              />
+            </label>
+            <label>
+              greaterThan:
+              <input
+                type="number"
+                name="greaterThan"
+                value={conn.greaterThan}
+                onChange={e => onChangeConn('greaterThan', e.target.value)}
+              />
+            </label>
+            <label className="inputList">
+              inArray:
+              <ReactListInput
+                initialStagingValue=""
+                onChange={value => onChangeConn('inArray', value)}
+                maxItems={20}
+                minItems={0}
+                ItemComponent={Item}
+                StagingComponent={StagingItem}
+                value={conn.inArray}
+              />
+            </label>
+            <label className="inputList">
+              notInArray:
+              <ReactListInput
+                initialStagingValue=""
+                onChange={value => onChangeConn('notInArray', value)}
+                maxItems={20}
+                minItems={0}
+                ItemComponent={Item}
+                StagingComponent={StagingItem}
+                value={conn.notInArray}
+              />
+            </label>
+            {hasIntents && (
+              <label className="inputList">
+                nlp:
+                <ReactListInput
+                  initialStagingValue={{ key: null, op: null, value: '' }}
+                  onChange={value =>
+                    onChangeConn('nlp', this.getFilterFromItems(value))
+                  }
+                  maxItems={20}
+                  minItems={0}
+                  ItemComponent={IntentFilterItemHOC(getIntents)}
+                  StagingComponent={StagingIntentFilterItemHOC(getIntents)}
+                  value={getFilterItems(conn.nlp)}
+                />
+              </label>
             )}
-            value={getFilterItems(conn.context)}
-          />
-        </label>
+            <label className="inputList">
+              answers:
+              <ReactListInput
+                initialStagingValue={{ key: null, op: null, value: '' }}
+                onChange={value =>
+                  onChangeConn('answers', this.getFilterFromItems(value))
+                }
+                maxItems={20}
+                minItems={0}
+                ItemComponent={FilterItemHOC(getPrevIndexes)}
+                StagingComponent={StagingFilterItemHOC(getPrevIndexes)}
+                value={getFilterItems(conn.answers)}
+              />
+            </label>
+            <label className="inputList">
+              context:
+              <ReactListInput
+                initialStagingValue={{ key: null, op: null, value: '' }}
+                onChange={value =>
+                  onChangeConn('context', this.getFilterFromItems(value))
+                }
+                maxItems={20}
+                minItems={0}
+                ItemComponent={FilterItemHOC(getSelectedNodePrevContextVars)}
+                StagingComponent={StagingFilterItemHOC(
+                  getSelectedNodePrevContextVars
+                )}
+                value={getFilterItems(conn.context)}
+              />
+            </label>
+          </div>
+        )}
         <label className="inputList">
           Set Context:
           <ReactListInput
