83c1c5a0145eba0a8062f830acfc427cc69e4cce
Diego Schvartzman <diego@holaemi.com>
2020-03-09 15:25:34 -0300

    help screen

--- src/examples/emi-flow-editor/index.js
+++ src/examples/emi-flow-editor/index.js
@@ -22,6 +22,9 @@ import 'brace/ext/searchbox';
 import 'brace/mode/json';
 import 'brace/theme/monokai';
 import { withAlert } from 'react-alert';
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
+
 import { type IEdge } from '../../components/edge';
 import GraphView from '../../components/graph-view';
 import { type INode } from '../../components/node';
@@ -734,6 +737,60 @@ class BwdlEditable extends React.Component<{}, IBwdlState> {
   faqClasses = () =>
     GraphUtils.classNames(this.state.faqSelected ? ['selected'] : []);
 
+  showHelpDialog = () => {
+    const customUI = ({ onClose }) => (
+      <div className="react-confirm-alert-body" style={{ width: '1000px' }}>
+        <h1>Help</h1>
+        <h2>Leyend</h2>
+        Not yet :(
+        <h2>Useful tips</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th>Feature</th>
+              <th>Input</th>
+            </tr>
+            <tr>
+              <td>Create node</td>
+              <td>SHIFT + Left Click</td>
+            </tr>
+            <tr>
+              <td>Create edge</td>
+              <td>
+                SHIFT + Left Click down over a node, then drag to other node
+              </td>
+            </tr>
+            <tr>
+              <td>Select node or edge</td>
+              <td>Left Click over a nodeor edge</td>
+            </tr>
+            <tr>
+              <td>Copy node or edge</td>
+              <td>Select a node or edge, then Ctrl + C</td>
+            </tr>
+            <tr>
+              <td>Paste node</td>
+              <td>After copying a node, just Ctrl + V</td>
+            </tr>
+            <tr>
+              <td>Paste edge</td>
+              <td>
+                After copying an edge, just Ctrl + V over an existing edge. The
+                edge will be replaced. Works with edges with multiple
+                connections.
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="react-confirm-alert-button-group">
+          <button onClick={onClose}>Ok!</button>
+        </div>
+      </div>
+    );
+
+    confirmAlert({ customUI });
+  };
+
   renderTextEditor() {
     return (
       <Sidebar
@@ -814,6 +871,20 @@ class BwdlEditable extends React.Component<{}, IBwdlState> {
           onPasteSelected={this.onPasteSelected}
           layoutEngineType={this.state.layoutEngineType}
         />
+        <svg
+          id="help"
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="far"
+          data-icon="question-circle"
+          className="svg-inline--fa fa-question-circle fa-w-16"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+          onClick={this.showHelpDialog}
+        >
+          <path d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 448c-110.532 0-200-89.431-200-200 0-110.495 89.472-200 200-200 110.491 0 200 89.471 200 200 0 110.53-89.431 200-200 200zm107.244-255.2c0 67.052-72.421 68.084-72.421 92.863V300c0 6.627-5.373 12-12 12h-45.647c-6.627 0-12-5.373-12-12v-8.659c0-35.745 27.1-50.034 47.579-61.516 17.561-9.845 28.324-16.541 28.324-29.579 0-17.246-21.999-28.693-39.784-28.693-23.189 0-33.894 10.977-48.942 29.969-4.057 5.12-11.46 6.071-16.666 2.124l-27.824-21.098c-5.107-3.872-6.251-11.066-2.644-16.363C184.846 131.491 214.94 112 261.794 112c49.071 0 101.45 38.304 101.45 88.8zM298 368c0 23.159-18.841 42-42 42s-42-18.841-42-42 18.841-42 42-42 42 18.841 42 42z"></path>
+        </svg>
         <svg
           id="faq"
           className={this.faqClasses()}
