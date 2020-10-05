35791820dab54056fe3dad4ae36ee4b7d58df21f
Keisuke Kan <9renpoto@gmail.com>
2020-10-05 22:42:56 +0900

    fix: webpack optimization

--- webpack.prod.js
+++ webpack.prod.js
@@ -81,11 +81,19 @@ module.exports = {
   },
 
   plugins: [
-    new TerserPlugin({ sourceMap: true }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     })
   ],
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: true,
+      }),
+    ],
+  },
 
   externals: {
     // TODO: figure out how to deal with externals
