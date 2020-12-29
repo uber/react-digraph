3a1f7315976d1bc012ff53de428d69a777312843
Allan Bogh <ajbogh@uber.com>
2020-12-29 15:31:28 -0800

    Updated dependencies to remove examples folder

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,10 @@ module.exports = {
   mode: 'development',
   context: __dirname + '/src',
   entry: {
-    main: './index.js',
+    main: '../../index.js',
     example: './examples/app.js',
     exampleCss: './examples/app.scss',
-    css: './styles/main.scss'
+    css: './styles/main.scss',
   },
 
   // cheap is not the best for quality, but it speeds up the build. the downside
@@ -26,20 +26,23 @@ module.exports = {
     path: __dirname + '/dist',
     publicPath: '/dist/',
     library: 'ReactDigraph',
-    libraryTarget: 'var' //'commonjs2' // ,
+    libraryTarget: 'var', //'commonjs2' // ,
     // libraryExport: 'default'
   },
 
   devServer: {
-    contentBase: [path.join(__dirname, 'src', 'examples'), path.join(__dirname, 'dist')],
+    contentBase: [
+      path.join(__dirname, 'src', 'examples'),
+      path.join(__dirname, 'dist'),
+    ],
     compress: true,
     port: 9000,
-    watchContentBase: true
+    watchContentBase: true,
   },
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: ['.ts', '.tsx', '.js', '.json']
+    extensions: ['.ts', '.tsx', '.js', '.json'],
   },
 
   module: {
@@ -51,7 +54,7 @@ module.exports = {
         loader: 'eslint-loader',
         options: {
           configFile: '.eslintrc',
-        }
+        },
       },
       {
         test: /\.jsx?$/,
@@ -62,7 +65,7 @@ module.exports = {
             options: {
               babelrc: true,
               // presets: ['es2015']
-            }
+            },
           },
           // Need to run the react preset first to strip flow annotations
           // {
@@ -73,13 +76,13 @@ module.exports = {
           //     plugins: ['transform-class-properties']
           //   }
           // }
-        ]
+        ],
       },
       // All files with a '.ts' or '.tsx' extension will be handled by
       // 'awesome-typescript-loader'.
       {
         test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader'
+        loader: 'awesome-typescript-loader',
       },
 
       // All output '.js' files will have any sourcemaps re-processed by
@@ -87,7 +90,7 @@ module.exports = {
       {
         enforce: 'pre',
         test: /\.js$/,
-        loader: 'source-map-loader'
+        loader: 'source-map-loader',
       },
 
       // All scss files
@@ -95,21 +98,21 @@ module.exports = {
         test: /\.s?css$/,
         use: [
           {
-            loader: 'style-loader' // creates style nodes from JS strings
+            loader: 'style-loader', // creates style nodes from JS strings
           },
           {
-            loader: 'css-loader' // translates CSS into CommonJS
+            loader: 'css-loader', // translates CSS into CommonJS
           },
           {
-            loader: 'sass-loader' // compiles Sass to CSS
-          }
-        ]
+            loader: 'sass-loader', // compiles Sass to CSS
+          },
+        ],
       },
       {
         test: /\.svg$/,
-        loader: 'svg-inline-loader'
-      }
-    ]
+        loader: 'svg-inline-loader',
+      },
+    ],
   },
 
   plugins: [
@@ -118,13 +121,13 @@ module.exports = {
     new CopyWebpackPlugin([
       {
         from: './examples/**/index.html',
-        to: 'index.html'
+        to: 'index.html',
       },
       {
         from: './examples/**/*.js',
-        to: '[name].js'
-      }
-    ])
+        to: '[name].js',
+      },
+    ]),
   ],
 
   externals: {
@@ -132,6 +135,6 @@ module.exports = {
     // react: 'React',
     // 'react-dom': 'ReactDOM',
     // d3: 'd3',
-    tslib: 'tslib'
-  }
+    tslib: 'tslib',
+  },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 
@@ -8,7 +7,7 @@ module.exports = {
   context: __dirname + '/src',
   entry: {
     main: './index.js',
-    css: './styles/main.scss'
+    css: './styles/main.scss',
   },
 
   output: {
@@ -17,12 +16,12 @@ module.exports = {
     path: __dirname + '/dist',
     publicPath: '/dist/',
     library: 'ReactDigraph',
-    libraryTarget: 'commonjs2'
+    libraryTarget: 'commonjs2',
   },
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: ['.ts', '.tsx', '.js', '.json']
+    extensions: ['.ts', '.tsx', '.js', '.json'],
   },
 
   module: {
@@ -34,7 +33,7 @@ module.exports = {
         loader: 'eslint-loader',
         options: {
           configFile: '.eslintrc',
-        }
+        },
       },
       {
         test: /\.jsx?$/,
@@ -46,16 +45,16 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              babelrc: true
-            }
+              babelrc: true,
+            },
           },
-        ]
+        ],
       },
       // All files with a '.ts' or '.tsx' extension will be handled by
       // 'awesome-typescript-loader'.
       {
         test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader'
+        loader: 'awesome-typescript-loader',
       },
 
       // All scss files
@@ -63,27 +62,27 @@ module.exports = {
         test: /\.s?css$/,
         use: [
           {
-            loader: 'style-loader' // creates style nodes from JS strings
+            loader: 'style-loader', // creates style nodes from JS strings
           },
           {
-            loader: 'css-loader' // translates CSS into CommonJS
+            loader: 'css-loader', // translates CSS into CommonJS
           },
           {
-            loader: 'sass-loader' // compiles Sass to CSS
-          }
-        ]
+            loader: 'sass-loader', // compiles Sass to CSS
+          },
+        ],
       },
       {
         test: /\.svg$/,
-        loader: 'svg-inline-loader'
-      }
-    ]
+        loader: 'svg-inline-loader',
+      },
+    ],
   },
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
   ],
 
   optimization: {
@@ -102,15 +101,15 @@ module.exports = {
       root: 'react',
       global: 'React',
       commonjs: 'react',
-      commonjs2: 'react'
+      commonjs2: 'react',
     },
     'react-dom': {
       amd: 'react-dom',
       root: 'react-dom',
       global: 'ReactDOM',
       commonjs: 'react-dom',
-      commonjs2: 'react-dom'
+      commonjs2: 'react-dom',
     },
-    tslib: 'tslib'
-  }
+    tslib: 'tslib',
+  },
 };
